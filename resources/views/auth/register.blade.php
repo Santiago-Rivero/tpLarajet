83550fa42f165792cabf35e5ba185bc72c683af5
Your Name <you@example.com>
2021-03-17 18:04:21 -0300

    warning:version bianca

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,10 +10,9 @@
             @csrf
 
             <div>
-                <x-jet-label for="name" value="{{ __('Nombre') }}" />
+                <x-jet-label for="name" value="{{ __('Name') }}" />
                 <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             </div>
-
 
             <div class="mt-4">
                 <x-jet-label for="email" value="{{ __('Email') }}" />
@@ -21,12 +20,12 @@
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Contraseña') }}" />
+                <x-jet-label for="password" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
             </div>
 
             <div class="mt-4">
-                <x-jet-label for="password_confirmation" value="{{ __('Confirmar Contraseña') }}" />
+                <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
                 <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
             </div>
 
@@ -49,19 +48,11 @@
 
             <div class="flex items-center justify-end mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('login') }}">
-<<<<<<< HEAD
-                    {{ __('¿Esta registrado?') }}
+                    {{ __('Already registered?') }}
                 </a>
 
                 <x-jet-button class="ml-4">
-                    {{ __('Registrese') }}
-=======
-                    {{ __('¿Ya estas registrado?') }}
-                </a>
-
-                <x-jet-button class="ml-4">
-                    {{ __('Registrar') }}
->>>>>>> 96aa2968014c7aecc86bce079d72c5a8566b45e7
+                    {{ __('Register') }}
                 </x-jet-button>
             </div>
         </form>
